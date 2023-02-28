f62a97e59543663a8b931b62e009a5afca4e6e57
Willi Buli <buliwilli17@gmail.com>
2023-02-28 20:06:30 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/WilliBuli17/app-say-hallo
+
+go 1.20
+
+require github.com/WilliBuli17/go-say-hallo/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_say_hallo "github.com/WilliBuli17/go-say-hallo/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hallo.SayHello("Willi"))
+}
